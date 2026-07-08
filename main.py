09bc14a9685402fc43d6a9d92d2938b01a78d181
yarik273 <yarik140994@gmail.com>
2026-07-08 13:42:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def send_cs_status(message):
     status_text = get_cs_status_direct()
     bot.reply_to(message, status_text, parse_mode="Markdown")
 
-if _name_ == "_main_":
+if  __name__ == "__main__":
     # Запуск веб-сервера для Render в отдельном потоке
     threading.Thread(target=run_web_server, daemon=True).start()
     print("Telegram bot started successfully...")
