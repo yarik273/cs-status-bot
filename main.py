009bdbac62fbc0709c10367afcd1271f1a2d32e9
yarik273 <yarik140994@gmail.com>
2026-09-02 10:35:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_HEAD(self):
         self.send_response(200)
         self.end_headers()   
-
-    def run_web_server():
+        
+def run_web_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
     server.serve_forever()
